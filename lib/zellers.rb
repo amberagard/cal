041b1dd3baff47a56602126f824a0c744465119b
Amber Agard <amber.n.agard@gmail.com>
2014-07-17 22:00:28 -0500

    zellers complete

--- lib/zellers.rb
+++ lib/zellers.rb
@@ -1,6 +1,14 @@
 
 class ZellersCongruence
   def self.calculate(month, day, year)
-
+    months = ["March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February"]
+    days = ["Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+    q = day
+    m = months.index(month.capitalize) + 3
+    y = (m <= 12) ? year : year - 1
+    k = y % 100
+    j = y / 100
+    h = (q + (13 * (m + 1)/5).floor + k + (k/4).floor + (j/4).floor + (5 * j)) % 7
+    days[h]
   end
 end
